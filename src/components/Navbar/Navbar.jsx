c15a10767fd9482040423b243307e628039af70a
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-11-23 00:12:20 +0530

    @nirmalyax

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <div className="nav-bar">
       <div className="heading">QR - Engine</div>
       <p>Create your own QR 
-      designed with ❤️ by&nbsp;<a target="_blank" href="https://nirmalyax.dev" >nirmalyax</a></p>
+      designed with ❤️ by&nbsp;<a target="_blank" href="https://nirmalyax.dev" >nirmalyax </a></p>
 
       <div className="icon">
         <FaGithub/>
